b2c12ae1023cacb0f2894efeaf6d492c43e5b61a
Sean <sean.alvarenga@gmail.com>
2012-10-27 21:35:13 -0200

    Adicionei menu do Admin e da Entidade

---
+++ classes/view/AdminMenu.php
@@ -0,0 +1,8 @@
+<?php
+	<ul>
+	  <li><a href="" >Visualizar Log</a></li>
+	  <li><a href="">Inserir Moderador</a></li>
+	  <li><a href="">Alterar Moderador</a></li>
+	  <li><a href="">Excluir Moderador</a></li>
+	</ul>
+?>
