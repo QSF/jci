c4d4f95cfef4ecfee761e69b5653982c595b44b1
viniciusffj <viniciusffj@gmail.com>
2012-10-29 21:11:36 -0200

    interface DAO criada

--- classes/model/dao/DAO.php
+++ classes/model/dao/DAO.php
@@ -1,23 +1,32 @@
 <?php 
+
+/** Interface geral do DAO.
+* Esta interface possui o CRUD básico utilizado no nosso sistema, sendo que qualquer DAO implementa ela.
+*
+*/
 interface DAO{
 	/** Método que persiste um objeto no banco.
 	*	@param $object objeto a ser persistido.
 	*/
 	public function insert($object);
 	/** Método que deleta uma tupla da tabela de acordo com o objeto passado por parâmetro.
-	*	@param $id id(chave primária) da tupla que será buscada.
-	*	@return object objeto referente a tupla com este id na tabela.
-	*	@return null caso não tenha nenhuma tupla com este id.
+	*	@param $object objeto que contém o id(chave primária) da tupla que será buscada.
+	*	@todo Jogar exceptions
 	*/
 	public function delete($id);
-	/** Método que persiste um objeto no banco.
-	*	@param $object objeto a ser persistido.
+	/** Método que atualiza um objeto no banco.
+	* 	Séra atualizada a tupla que contém o id do objeto passado, com os valores dete objeto.
+	*	@param $object objeto a ser atualizado.
+	*
+	*	@todo Jogar exceptions	
 	*/
 	public function update($object);
-	/** Método que retorna o objeto equivalente à uma coluna do banco.
+	/** Método que retorna o objeto equivalente à uma coluna do banco que possui o id passado.
 	*	@param $id id(chave primária) da tupla que será buscada.
 	*	@return object objeto referente a tupla com este id na tabela.
 	*	@return null caso não tenha nenhuma tupla com este id.
+	*
+	*	@todo Jogar exceptions	
 	*/
 	public function findById($id);
 }
