d38671925df80fd2e5a07c25bee768369f35a06e
gjhenrique <gjhenrique@gmail.com>
2012-10-30 02:55:03 -0200

    Arrumando echo que estava faltando nos links do menu do form

--- classes/view/pages/menu/MenuForms.php
+++ classes/view/pages/menu/MenuForms.php
@@ -1,11 +1,11 @@
-<a href=<?php PUBLIC_PATH;?>"/index.php?controller=registration&action=directDisplay&page=NPVolunteerForm">
+<a href=<?php echo PUBLIC_PATH;?>"/index.php?controller=registration&action=directDisplay&page=NPVolunteerForm">
 	Formulário Do Voluntário Pessoa Fisica
 </a>
 <br/>
-<a href=<?php PUBLIC_PATH;?>"/index.php?controller=registration&action=directDisplay&page=LPVolunteerForm">
+<a href=<?php echo PUBLIC_PATH;?>"/index.php?controller=registration&action=directDisplay&page=LPVolunteerForm">
 	Formulário Do Voluntário Pessoa Jurídica
 </a>
 <br/>
-<a href=<?php PUBLIC_PATH;?>"/index.php?controller=registration&action=directDisplay&page=EntityForm">
+<a href=<?php echo PUBLIC_PATH;?>"/index.php?controller=registration&action=directDisplay&page=EntityForm">
 	Formulário Do Voluntário Pessoa Jurídica
 </a>
