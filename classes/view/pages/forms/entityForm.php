02e3a013a2e57f022a8afb757aac0e5118462511
Leticia <leticia@leticia-note.(none)>
2012-11-04 14:43:57 -0200

    primeira formatação dos forms, foi apenas colocados com tabela para fazer a formatação

--- classes/view/pages/forms/entityForm.php
+++ classes/view/pages/forms/entityForm.php
@@ -1,11 +1,25 @@
-	<label for="stablishmentDate">Data de fundação</label>
-	<input type="text" 
+	<table width="40%">
+		<tr>
+			<td width=70%><label for="stablishmentDate">Data de fundação</label>
+</td>
+			<td width=50%><input type="text" 
 	value="<?php if($user->getEstablishmentDate()) echo $user->getEstablishmentDate()->format('d-m-Y') ?>" 
 	id="idStablishmentDate" name="establishmentDate"/><br/>
-
-	<label for="site">Site</label>
-	<input type="text" id="idSite" name="site" value="<?php echo $user->getSite() ?>" /><br/>
-
-	<input type="checkbox" name="receivedNewsletter" value="yes"
-	<?php if($user-> getNewsletter()) echo "checked=yes"?>/>
-	Receber notícias da JCI por email<br/>
+</td>
+		</tr>
+		<tr>
+			<td width=70%><label for="site">Site</label></td>
+			<td width=50%><input type="text" id="idSite" name="site" value="<?php echo $user->getSite() ?>" /><br/>
+</td>
+		</tr>
+		<tr>
+			<td width=70%><input type="checkbox" name="receivedNewsletter" value="yes"
+	</td>
+			<td width=50%><?php if($user-> getNewsletter()) echo "checked=yes"?>
+	Receber notícias da JCI por email<br/></td>
+		</tr>
+	</table>
+		
+	
+	
+	
--- classes/view/pages/forms/volunteerForm.php
+++ classes/view/pages/forms/volunteerForm.php
@@ -1,4 +1,8 @@
-	<label for="experience">Se já teve alguma experiência com serviço voluntário, em quais atividades?</label><br/>
-	<input type="text" id="idExperience" name="experience" value="<?php echo $user->getExperience()?>"><br/><br/>
+	<table width="40%">
+		<tr>
+			<td width=70%><label for="experience">Se já teve alguma experiência com serviço voluntário, em quais atividades?</label><br/></td>
+			<td width=50%>	<input type="text" id="idExperience" name="experience" value="<?php echo $user->getExperience()?>"><br/><br/>
+</td>
+		</tr>
 
 	
