17e545539cca09383212d86438f1f2a992bfac37
gjhenrique <gjhenrique@gmail.com>
2012-11-06 02:08:26 -0200

    Adicionado subcampos gerados dinamicamente

--- classes/view/pages/forms/listFieldsCheckbox.php
+++ classes/view/pages/forms/listFieldsCheckbox.php
@@ -1,16 +1,27 @@
 <ul>
 <?php
+	$arrayFieldsChild = array();
 	//para cada campo macro chama o listar campos
-	foreach ($fields as $var) {?>
-		<li> <?php listFields($var,$user);
-	}
-?>
-</ul>
+		foreach ($fields as $var) {?>
+		<li>
+		<?php  $arrayFieldsChild[$var->getId()] = listFields($var,$user);
 
-<?php 
+		} ?>
+
+
+</ul>
+<div id="childFields"> </div>
+
+<script type="text/javascript"> var arrayJSONChildren = 
+<?php echo utf8_encode(json_encode($arrayFieldsChild));?> </script>
+
+<?php
+
 function listFields($var,$user){
+	$arrayFields = array(); 
 	?>
-	<input type="checkbox" name="actingArea[]" value="<?php echo $var->getID()?>" 
+	<input type="checkbox" name="actingArea[]" value="<?php echo $var->getID();?>" 
+	id="checkbox<?php echo $var->getId()?>" class="actingArea"
 	<?php if( hasId($var->getId(),$user->getActingArea()) ){
 		echo "checked=yes";
 	}
@@ -18,10 +29,10 @@ function listFields($var,$user){
 	<?php echo $var->getName() ?>
 	<ul>
 	<?php 
-		foreach ($var->getChildren() as $child){ ?>
-			<li><?php listFields($child,$user); 
-		}
-	?>
-	</ul> <?php 
-} 
-?>
+		foreach ($var->getChildren() as $child){
+			array_push($arrayFields, array("id" => $child->getId(), "name" => $child->getName()));
+		}  ?>
+	</ul>  
+
+	<?php 
+	return $arrayFields; } ?>
