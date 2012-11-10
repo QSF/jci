106ca850b4ba5f4dd98dfec106217b6452702b33
Gustavo Scaloni Vendramini <vendramini.gu@gmail.com>
2012-11-10 19:01:17 -0200

    Tirando sublinhado dos links no listUser tinha esquecido, OBS: ta no html tem selecionar com css

--- classes/view/pages/NewsList.php
+++ classes/view/pages/NewsList.php
@@ -6,7 +6,7 @@
         <a style="text-decoration: none;" href="./index.php?controller=news&action=redirectUpdate&news_id=<?php echo $newsElem->getId()?>">    <i class="icon-edit"></i><!-- <button class="btn btn-primary">Editar</button> --> </a>
         <a style="text-decoration: none;" href="./index.php?controller=news&action=deleteNews&news_id=<?php echo $newsElem->getId()?>">  <i class="icon-remove"></i><!-- <button class="btn btn-primary">Deletar</button> -->  </a>
         <label>
-          <a style="text-decoration: none;" href="./index.php?controller=news&action=get&news_id=<?php echo $newsElem->getId()?>"><strong><?php echo $newsElem->getTitle()?></strong></a>
+          <a style="text-decoration: none;" href="./index.php?controller=news&action=get&news_id=<?php echo $newsElem->getId()?>"><?php echo $newsElem->getTitle()?></a>
         </label>
       </span><br/><hr>
 <?php } //end foreach dos usuarios?>
