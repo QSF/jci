77f636a88355466a36aea86f8ff2b02919ee752f
Gustavo Scaloni Vendramini <vendramini.gu@gmail.com>
2012-11-10 18:16:02 -0200

    Aplicado CSS na lista de usuários e notícias

--- classes/view/pages/NewsList.php
+++ classes/view/pages/NewsList.php
@@ -1,21 +1,17 @@
 <h3>Lista de Notícias</h3>
 
 <?php include HELPER_PATH."/Pagination.php";?>
-<?php foreach($news as $newsElem){ ?>
-	
-	<a href="./index.php?controller=news&action=get&news_id=<?php echo $newsElem->getId()?>">
-		<?php echo $newsElem->getTitle()?>
-	</a>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<a href="./index.php?controller=news&action=redirectUpdate&news_id=<?php echo $newsElem->getId()?>">
-		Editar
-	</a>
-	&nbsp;&nbsp;
-	<a href="./index.php?controller=news&action=deleteNews&news_id=<?php echo $newsElem->getId()?>">
-		Deletar
-	</a>
-	&nbsp;
-	<br/>
-	<?php } //end foreach dos usuarios?>
+    <?php foreach($news as $newsElem){ ?>
+      <span class="manage-list">
+        <a style="text-decoration: none;" href="./index.php?controller=news&action=redirectUpdate&news_id=<?php echo $newsElem->getId()?>">    <i class="icon-edit"></i><!-- <button class="btn btn-primary">Editar</button> --> </a>
+        <a style="text-decoration: none;" href="./index.php?controller=news&action=deleteNews&news_id=<?php echo $newsElem->getId()?>">  <i class="icon-remove"></i><!-- <button class="btn btn-primary">Deletar</button> -->  </a>
+        <label>
+          <a style="text-decoration: none;" href="./index.php?controller=news&action=get&news_id=<?php echo $newsElem->getId()?>"><strong><?php echo $newsElem->getTitle()?></strong></a>
+        </label>
+      </span><br/><hr>
+<?php } //end foreach dos usuarios?>
 
-	<?php echoPagination($pagesNum, $currentPage, $url);?>
+<?php echoPagination($pagesNum, $currentPage, $url);?>
+
+
+
--- classes/view/pages/UsersList.php
+++ classes/view/pages/UsersList.php
@@ -2,28 +2,30 @@
 
 <?php include HELPER_PATH."/Pagination.php";?>
 <?php foreach($users as $user){ ?>
-	
+  <span class="manage-list">
+	<label>
 	<a href="./index.php?controller=registration&action=read&user_id=<?php echo $user->getId()?>
 				&profile=<?php echo get_class($user)?>">
 		<?php echo $user->getName()?>
 	</a>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	</label>
+	
 	<a href="././index.php?controller=registration&action=redirectUserUpdate&user_id=<?php echo $user->getId()?>
 		&form=<?php echo get_class($user)?>">
-		Editar
+		<i class="icon-edit"></i>
 	</a>
-	&nbsp;&nbsp;
+	
 	<a href="./index.php?controller=registration&action=redirectUserDelete&user_id=<?php echo $user->getId();?>
 		&user_type=<?php echo get_class($user);?>">
-		Deletar
+		<i class="icon-remove"></i>
 	</a>
-	&nbsp;&nbsp;
+	
 	<?php if(isset($validateAction) && $validateAction === true) {?>
 	<a href="./index.php?controller=moderator&action=validateEntity&user_id=<?php echo $user->getId()?>">
 		Validar
 	</a>
 	<?php } ?>
-	<br/>
+	 </span><br/><hr>
 	<?php } //end foreach dos usuarios?>
 
 	<?php echoPagination($pagesNum, $currentPage, $url);?>
