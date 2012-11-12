4445f47910bdd0b1b0ff3627f16ce744a92ce82f
Gustavo Scaloni Vendramini <vendramini.gu@gmail.com>
2012-11-12 17:00:31 -0200

    Corrigido bug CSS: Listar uma notícia com um título muito grande fica estranho.

--- classes/view/pages/NewsList.php
+++ classes/view/pages/NewsList.php
@@ -2,13 +2,12 @@
 
 <?php include HELPER_PATH."/Pagination.php";?>
     <?php foreach($news as $newsElem){ ?>
-      <span class="manage-list">
-        <a style="text-decoration: none;" href="./index.php?controller=news&action=redirectUpdate&news_id=<?php echo $newsElem->getId()?>">    <i class="icon-edit"></i><!-- <button class="btn btn-primary">Editar</button> --> </a>
-        <a style="text-decoration: none;" href="./index.php?controller=news&action=deleteNews&news_id=<?php echo $newsElem->getId()?>">  <i class="icon-remove"></i><!-- <button class="btn btn-primary">Deletar</button> -->  </a>
-        <label>
+        <div class="grid_8 news">
+          <a style="text-decoration: none;" href="./index.php?controller=news&action=redirectUpdate&news_id=<?php echo $newsElem->getId()?>">    <i class="icon-edit"></i><!-- <button class="btn btn-primary">Editar</button> --> </a>
+          <a style="text-decoration: none;" href="./index.php?controller=news&action=deleteNews&news_id=<?php echo $newsElem->getId()?>">  <i class="icon-remove"></i><!-- <button class="btn btn-primary">Deletar</button> -->  </a>
           <a style="text-decoration: none;" href="./index.php?controller=news&action=get&news_id=<?php echo $newsElem->getId()?>"><?php echo $newsElem->getTitle()?></a>
-        </label>
-      </span><br/><hr>
+        </div>
+        <br/><hr>
 <?php } //end foreach dos usuarios?>
 
 <?php echoPagination($pagesNum, $currentPage, $url);?>
