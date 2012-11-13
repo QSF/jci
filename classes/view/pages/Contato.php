04533b53fcea6d314a346189d1c3cbb1891abb68
Gustavo Scaloni Vendramini <vendramini.gu@gmail.com>
2012-11-13 09:45:09 -0200

    Rodapé adicionado, página contato editada

--- classes/view/pages/Contato.php
+++ classes/view/pages/Contato.php
@@ -2,34 +2,29 @@
 
 <p>As reuniões da JCI Londrina são realizadas às segundas-feiras às 19:10 na Associação Comercial e Industrial de Londrina - ACIL. Para mais informações, não hesite em entrar em contato.</p>
 
-
-<form action="./index.php?controller=guest&action=sendMail" method="post">
-<table>
-	<tr>
-		<td>
-			<label for="name">Nome</label>
-		</td>
-		
-		<td>
-			<input type="text" id="name" name="name" required="required"/>
-		</td>
-
-	<tr>
-		<td>
-			<label for="email">E-Mail</label>
-		</td>
-		<td>
-			<input type="email" id="email" name="email" required="required"/>
-		</td>
-	</tr>
-	
-</table>
 <br/>
-	<label for="content">Conteúdo</label>
-
-
-	<textarea name="content" style="width:500px; height:200px; margin-top: 2px; margin-bottom: 2px;" 
-		id="content" required="required"></textarea>
-<br/>
-<input type="submit" value="Enviar"/>
+<form class="form-horizontal" action="./index.php?controller=guest&action=sendMail" method="post">
+  <div class="control-group">
+    <label class="control-label" for="name">Nome</label>
+    <div class="controls">
+      <input type="text" id="name" name="name" required="required" placeholder="Digite seu nome." style="width:350px;">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="email">E-mail</label>
+    <div class="controls">
+      <input type="email" id="email" name="email" required="required" placeholder="E-mail" style="width:350px;">
+    </div>
+  </div>
+  <div class="control-group">
+  	<label class="control-label" for="content">Conteúdo</label>
+    <div class="controls">
+      <textarea name="content" id="content" required="required" rows="6" placeholder="Escreva sua mensagem aqui." style="width:350px;"></textarea>
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn">Enviar</button>
+    </div>
+  </div>
 </form>
