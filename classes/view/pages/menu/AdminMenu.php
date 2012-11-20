c6c5595b3de001720dfd9fdae45bc65eb37573fe
viniciusffj <viniciusffj@gmail.com>
2012-11-20 12:07:42 -0200

    Registrando bug na hora de editar uma doação

--- classes/view/pages/menu/AdminMenu.php
+++
@@ -1,6 +0,0 @@
-<ul>
-	<?php require_once(PAGES_PATH . '/menu/AccountMenu.php');?>
-	<li><a href="./index.php?controller=registration&action=directDisplay&page=ModeratorForm">Cadastrar moderador</a></li>
-	<li><a href="">Exibir Moderadores(editar e excluir)	</a></li>
-	<li><a href="./index.php?controller=login&action=logout">Logout</a></li>
-</ul>
